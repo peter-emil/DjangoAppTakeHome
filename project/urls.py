a86168d91cd5b62dea4ab98900319136f16b4c0c
Peter Emil <pe.peteremil@gmail.com>
2019-12-15 23:07:36 +0200

    Added endpoint to refresh access token using the refresh token

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
 import accounts.views
 
 urlpatterns = [  # pylint: disable=invalid-name
@@ -7,5 +8,6 @@ urlpatterns = [  # pylint: disable=invalid-name
     path('accounts/', include([
         path('create/', accounts.views.CreateAccountView.as_view()),
         path('login/', accounts.views.LoginView.as_view()),
+        path('tokenrefresh/', TokenRefreshView.as_view()),
     ])),
 ]
