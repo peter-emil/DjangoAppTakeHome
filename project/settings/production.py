a2b657130e44e507333380d8b2e55bd6de4c23ab
Peter Emil <pe.peteremil@gmail.com>
2019-12-16 19:45:41 +0200

    Added settings definition for production, including database and S3 storage
    Added cloudformation template for demo deployment

--- project/settings/production.py
+++ project/settings/production.py
@@ -2,9 +2,32 @@
 from .base import *  # noqa: F403, F401
 
 
+INSTALLED_APPS.insert(6, 'storages')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa: F405
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': '3306',
+        'OPTIONS': {
+            # 'isolation_level': 'read committed',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
+AWS_STORAGE_BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'couponwebsite.storage_backends.MediaStorage'
+
+allowed_hosts = os.environ["ALLOWED_HOSTS"].split(";")
+ALLOWED_HOSTS = allowed_hosts
